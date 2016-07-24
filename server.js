9865338c73f4e2019f5d439e13119732f2488d30
danyparc <danyparc@gmail.com>
2016-07-24 00:40:47 -0500

    Se agregó favicon y browserify y babelify a los modulos

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ var express = require('express');
 
 var app = express();
 
-app.get('/',function(req, res) {
-	res.send('Hola Mundo');
-});
+app.set('view engine', 'pug');
 
-app.listen(3000, function(err){
-	if (err) return console.log('Hubo un error'), process.exit(1);
+app.use(express.static('public'));
 
-	console.log('Escuchando en el puerto 3000');
+app.get('/', function (req, res) {
+  res.render('index');
+})
+
+app.listen(3000, function (err) {
+  if (err) return console.log('Hubo un error'), process.exit(1);
+
+  console.log('Platzigram escuchando en el puerto 3000');
 })
---
+++ src/index.js
@@ -0,0 +1,5 @@
+var numeros = [400, 200, 1, -23];
+
+var numerosmas1 = numeros.map(n => n+1)
+
+console.log(numerosmas1);
